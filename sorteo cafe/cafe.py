59eae0ee5b4a621d1b9c41eedf6c2deb7f3208bd
Mateo <mateo.castano+1-at-621963248403>
2019-08-20 10:02:01 -0500

    Coffe Alexa Skill

---
+++ sorteo cafe/cafe.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.launch
+def launch():
+    welcome_text = render_template('welcome_text')
+    return question(welcome_text)
+
+@ask.intent('AMAZON.FallbackIntent')
+def fallback():
+    reprompt_text = render_template('ask_name_reprompt')
+    return question(rempromt_text)
+
+@ask.intent('CafeIntent')
+def hello(firstname):
+    if firstname is None:
+        #Si ningun nombre ha sido enviado
+        ask_name_text = render_template('ask_name')
+        return question(ask_name_text)
+    response_text = render_template('hello', firstname=firstname)
+    return statement(response_text).simple_card('Hola', response_text)
+
+if __name__=='__main__'
+    app-run(debug=True)
